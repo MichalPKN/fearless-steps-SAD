bb01a0e1e963038fda99a107f2ab56a752796111
Michal_Pokorny <michal.pokorny@tul.cz>
2024-11-25 00:02:43 +0100

    testing

--- job_script.sh
+++ job_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -q gpu@pbs-m1.metacentrum.cz
-#PBS -l walltime=9:0:0
-#PBS -l select=1:ncpus=1:ngpus=1:mem=32gb:scratch_ssd=32gb:cuda_version=12.6
+#PBS -l walltime=8:0:0
+#PBS -l select=1:ncpus=1:ngpus=1:mem=32gb:scratch_ssd=50gb:cuda_version=12.6
 #PBS -N Training_script
 DATADIR=/storage/brno2/home/miapp/fearless-steps-SAD/fearless-steps-SAD
 chmod 700 $SCRATCHDIR
@@ -16,7 +16,7 @@ nvidia-smi
 echo $CUDA_VISIBLE_DEVICES
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 echo "cuda checked"
-> $DATADIR/print_outputs_new2.log
-python3 -u __init__.py --datadir $SCRATCHDIR >> $DATADIR/print_outputs_new2.log
+> $DATADIR/print_outputs_new.log
+python3 -u __init__.py --datadir $SCRATCHDIR >> $DATADIR/print_outputs_new.log
 cp -R outs $DATADIR/outs/$JOID
 clean_scratch
