7e68968018627e262658fbdeea97fd6b2b2615a7
MichalPKN <michal.pokorny24@gmail.com>
2025-01-30 01:18:17 +0100

    re-test overlap bidir

--- RNN-train.py
+++ RNN-train.py
@@ -96,7 +96,7 @@ gc.collect()
 # training
 test_num = 1
 for f_test in range(1):
-    for batch_size, audio_size, overlap in [[10, 1000, 2]]:
+    for batch_size, audio_size, overlap in [[10, 1000, 500], [10, 1000, 200], [10, 500, 100], [10, 2000, 1000], [10, 2000, 400]]:
         print(f"\nsplitting, padding, etc. all data to batch size {batch_size}, audio size {audio_size}, overlap {overlap}")
         X, Y, masks = split_file(X_loaded, Y_loaded, seq_size=audio_size, overlap=overlap, shuffle=False) #TODO: use in all
         dataset = SADDataset(X, Y, masks)
