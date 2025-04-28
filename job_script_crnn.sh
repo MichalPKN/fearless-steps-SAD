27c4c55a8631e52780f1172869c13020d6d31669
Michal_Pokorny <michal.pokorny@tul.cz>
2025-04-28 11:27:53 +0200

    crnn test

--- job_script_crnn.sh
+++ job_script_crnn.sh
@@ -4,7 +4,7 @@
 #PBS -l select=1:ncpus=1:ngpus=1:mem=40gb:scratch_ssd=50gb:cuda_version=12.6
 #PBS -N crnn_training
 DATADIR=/storage/brno2/home/miapp/fearless-steps-SAD/fearless-steps-SAD
-LOGFILE=$DATADIR/out_crnn_10.log
+LOGFILE=$DATADIR/out_crnn_11.log
 chmod 700 $SCRATCHDIR
 cp -r $DATADIR/* $SCRATCHDIR
 module add mambaforge
